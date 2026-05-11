35417c07a9d8639869414bd3a7d5e50118ddde7f
Davi Campaner <campanerdavi@gmail.com>
2026-05-11 14:44:38 -0300

    feat: add WordController with POST endpoint stub

--- src/main/java/wordkeep/apiEnglish/controller/WordController.java
+++ src/main/java/wordkeep/apiEnglish/controller/WordController.java
@@ -4,13 +4,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import wordkeep.apiEnglish.word.DadosCadastroWord;
 
 @RestController
 @RequestMapping("words")
 public class WordController {
 
     @PostMapping
-    public void cadastrar(@RequestBody String json){
-        System.out.println(json);
+    public void cadastrar(@RequestBody DadosCadastroWord dados){
+        System.out.println(dados);
     }
 }
