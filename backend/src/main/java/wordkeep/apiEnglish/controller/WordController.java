10dfdae5660e44327aec650a0446fc1da114d9fb
Davi Campaner <campanerdavi@gmail.com>
2026-05-11 19:30:34 -0300

    fix: rename listaId to deckId and remove unused LocalDate import in DadosCadastroWord

--- backend/src/main/java/wordkeep/apiEnglish/controller/WordController.java
+++ backend/src/main/java/wordkeep/apiEnglish/controller/WordController.java
@@ -1,17 +1,23 @@
 package wordkeep.apiEnglish.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wordkeep.apiEnglish.word.DadosCadastroWord;
+import wordkeep.apiEnglish.word.Word;
+import wordkeep.apiEnglish.word.WordRepository;
 
 @RestController
 @RequestMapping("words")
 public class WordController {
 
+    @Autowired
+    private WordRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroWord dados){
-        System.out.println(dados);
+        repository.save(new Word(dados));
     }
 }
--- backend/src/main/java/wordkeep/apiEnglish/word/DadosCadastroWord.java
+++ backend/src/main/java/wordkeep/apiEnglish/word/DadosCadastroWord.java
@@ -1,6 +1,4 @@
 package wordkeep.apiEnglish.word;
 
-import java.time.LocalDate;
-
-public record DadosCadastroWord(String word, String translation, String sourceLanguage,String targetLanguage, Long listaId) {
+public record DadosCadastroWord(String word, String translation, String sourceLanguage, String targetLanguage, Long deckId) {
 }
