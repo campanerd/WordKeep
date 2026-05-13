cac9e93e2ebf286da0fe2841264af874f9c17518
Davi Campaner <campanerdavi@gmail.com>
2026-05-13 16:46:06 -0300

    fix: adjust ip config

---
+++ backend/src/main/java/wordkeep/apiEnglish/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package wordkeep.apiEnglish.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5500",
+                        "http://127.0.0.1:5500",
+                        "http://192.168.1.42:5500"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
