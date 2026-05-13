7e220ae027ef136b7312d6614831c1c18c38b886
Vinícius Regazio <viniciusregazio@icloud.com>
2026-05-13 16:10:35 -0300

    feat: testing connect frontend and backend

---
+++ frontend/src/script.js
@@ -0,0 +1,51 @@
+const button = document.getElementById("sendButton");
+
+button.addEventListener("click", async () => {
+
+    const word = document.getElementById("word").value;
+    const translation = document.getElementById("translation").value;
+
+    await fetch("http://localhost:8080/words", {
+
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+            word: word,
+            translation: translation,
+            sourceLanguage: "en",
+            targetLanguage: "pt"
+        })
+
+    });
+
+    carregarPalavras();
+
+});
+
+async function carregarPalavras(){
+
+    const response = await fetch("http://localhost:8080/words");
+
+    const words = await response.json();
+
+    const lista = document.getElementById("lista");
+
+    lista.innerHTML = "";
+
+    words.forEach(word => {
+
+        lista.innerHTML += `
+            <li>
+                ${word.word} - ${word.translation}
+            </li>
+        `;
+
+    });
+
+}
+
+carregarPalavras();
