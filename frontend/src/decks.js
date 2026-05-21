e279b7b0e02fe8e6e148843d7bc21948faf1aa15
Vinícius Regazio <viniciusregazio@icloud.com>
2026-05-21 14:48:36 -0300

    feat(frontend): add link to visualizarDecks and enhance deck list display with edit/delete options

--- frontend/src/decks.js
+++ frontend/src/decks.js
@@ -1,10 +1,12 @@
+import { API_URL } from "./config.js";
+
 const button = document.getElementById("sendButton");
 
 button.addEventListener("click", async () => {
 
     const name = document.getElementById("deck").value;
 
-    await fetch("http://192.168.1.42:8080/decks", {
+    await fetch(`${API_URL}/decks`, {
 
         method: "POST",
 
@@ -22,38 +24,64 @@ button.addEventListener("click", async () => {
 
 });
 
-async function carregarDecks(){
-
-    const response = await fetch("http://192.168.1.42:8080/decks");
-
+async function carregarDecks() {
+    const response = await fetch(`${API_URL}/decks`);
     const decks = await response.json();
-
     const deckList = document.getElementById("deckList");
 
     deckList.innerHTML = "";
 
     decks.forEach(deck => {
-
         deckList.innerHTML += `
-            <li>
-                ${deck.name}
+            <li style="display: flex; justify-content: space-between; align-items: center;">
+                <span>${deck.name}</span>
+                <div class="acoes">
+                    <button class="editdelete" style="background-color: #87CEEB ; margin: 10px;" onclick="editarDeck(${deck.id})">Editar</button>
+                    <button class="editdelete" style="background-color: #CD5C5C;" onclick="deletarDeck(${deck.id})">Excluir</button>
+                </div>
             </li>
         `;
+    });
+}
+    window.editarPalavra = async (id) =>  {
+
+    const novoNome = prompt("Novo nome:");
+
+    if (!novoNome) return;
+
+    await fetch(`${API_URL}/decks`, {
+
+        method: "PUT",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+            id: id,
+            name: novoNome
+        })
 
     });
+
+    carregarDecks();
+}
+
+  window.deletarDeck = async (id) =>  {
+
+    const confirmar = confirm("Deseja excluir esse deck?");
+
+    if (!confirmar) return;
+
+    await fetch(`${API_URL}/decks/${id}`, {
+
+        method: "DELETE"
+
+    });
+
+    carregarDecks();
 
 }
 
-    lista.innerHTML = "";
-
-    words.forEach(word => {
-
-        lista.innerHTML += `
-            <li>
-                ${word.word} - ${word.translation}
-            </li>
-        `;
-
-    });
 
 carregarDecks();
